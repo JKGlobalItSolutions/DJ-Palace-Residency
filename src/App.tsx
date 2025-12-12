9ce23ba5a943e28dbfe2e625c4ed392bbb46b591
SUDHARSAN <kkqqq77288@gmail.com>
2025-12-12 14:45:44 +0530

    /

--- src/App.tsx
+++ src/App.tsx
@@ -7,7 +7,7 @@ const queryClient = new QueryClient();
 
 const App = () => (
   <QueryClientProvider client={queryClient}>
-    <BrowserRouter basename="/DJ-Palace-Residency">
+    <BrowserRouter basename="/">
       <Routes>
         <Route path="/" element={<Index />} />
         {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
--- vite.config.ts
+++ vite.config.ts
@@ -15,7 +15,7 @@ export default defineConfig(({ mode }) => ({
     },
   },
 
-  base: "/DJ-Palace-Residency/",
+  base: "/",
 
   plugins: [
     react(),
